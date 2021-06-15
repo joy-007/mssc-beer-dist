274e3510537614b71c0c1d21ec469cb2c9e575e6
joy-007 <66425148+joy-007@users.noreply.github.com>
2021-06-15 10:55:46 +0530

    Update BeerServiceImpl.java

--- mssc-beer/src/main/java/com/souvik/msscbeer/service/BeerServiceImpl.java
+++ mssc-beer/src/main/java/com/souvik/msscbeer/service/BeerServiceImpl.java
@@ -10,13 +10,31 @@ public class BeerServiceImpl implements BeerService{
 	@Override
 	public BeerDto getBeerById(int beerId) {
 	
-		System.out.println("Reached service layer123");
+		System.out.println("Reached service layer");
 		BeerDto beer = new BeerDto();
 		beer.setId(beerId);
 		beer.setBeerName("Kingfisher");
 		beer.setBeerStyle("Bottle");
 		beer.setUpc(100.50);
 		return beer;
+	}
+
+	@Override
+	public void updateBeer(int beerId, BeerDto beerDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveBeer(BeerDto beerDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBeerById(int beerId) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
