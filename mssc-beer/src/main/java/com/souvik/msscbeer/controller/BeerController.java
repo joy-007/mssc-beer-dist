d74c11d15bd42dd084cc2dfcadba2d82f1fc56f9
joy-007 <66425148+joy-007@users.noreply.github.com>
2021-06-15 10:55:22 +0530

    Update BeerController.java
    check

--- mssc-beer/src/main/java/com/souvik/msscbeer/controller/BeerController.java
+++ mssc-beer/src/main/java/com/souvik/msscbeer/controller/BeerController.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,16 +22,40 @@ public class BeerController {
 	@Autowired
 	private BeerService beerService;
 	
-	/*public BeerController(BeerService beerService) {
-		this.beerService = beerService;
-	}*/
-
 	
 	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") int beerId){
 				
+		
 		return new ResponseEntity<>(beerService.getBeerById(beerId),HttpStatus.OK);
 		
 	}
+	
+	@PostMapping
+	public ResponseEntity<BeerDto> saveBeer(@RequestBody BeerDto beerDto){
+		
+		beerService.saveBeer(beerDto);
+		
+		return new ResponseEntity<BeerDto>(HttpStatus.CREATED);
+	}
+	
+	@PatchMapping("/{beerId}")
+	public ResponseEntity<BeerDto> updateBeer(@PathVariable("beerId") int beerId,@RequestBody BeerDto beerDto){
+		
+		beerService.updateBeer(beerId,beerDto);
+		
+		return new ResponseEntity<BeerDto>(HttpStatus.OK);
+	}
+	
+	
+	@PatchMapping("/{beerId}")
+	public ResponseEntity<BeerDto> deleteBeer(@PathVariable("beerId") int beerId){
+		
+		beerService.deleteBeerById(beerId);
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	
 
 }
